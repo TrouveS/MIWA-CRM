eb5b9a8faf7560b4348b07c2ef2ffb37ebd6fe6b
Luxia25 <alexandra.pizzini@gmail.com>
2016-01-11 17:41:14 +0100

    Update client

--- app/Global.java
+++ app/Global.java
@@ -12,32 +12,6 @@ public class Global extends GlobalSettings {
             Logger.info("start in dev mode");
         //TODO on start action
         super.onStart(application);
-        JSONObject json = new JSONObject();
-        json.put("nom","Flantier");
-        json.put("prenom","Noelle");
-        json.put("client_id_local","142");
-        json.put("magasin_id","1");
-        json.put("date_de_naissance", "20/08/1923");
-        json.put("sexe", "F");
-        json.put("email", "noel@flantier.fr");
-        json.put("idRue", "rue du bois fleuri");
-        json.put("idVille", "Reims");
-        json.put("idCodePostal","51100");
-
-        JSONObject json2 = new JSONObject();
-        json2.put("nom","Flaner");
-        json2.put("prenom","Noel");
-        json2.put("client_id_local","142");
-        json2.put("magasin_id","1");
-        json2.put("date_de_naissance", "20/08/1923");
-        json2.put("sexe", "F");
-        json2.put("email", "noel@flaner.fr");
-        json2.put("idRue", "rue du bois fleuri");
-        json2.put("idVille", "Reims");
-        json2.put("idCodePostal","51100");
-
-        new ClientMessagePojo().action(json);
-        new ClientMessagePojo().action(json2);
     }
 
 }
--- app/controllers/utils/pojo/AsyncMessagePojo/ClientMessagePojo/ClientMessagePojo.java
+++ app/controllers/utils/pojo/AsyncMessagePojo/ClientMessagePojo/ClientMessagePojo.java
@@ -1,59 +1,129 @@
 package controllers.utils.pojo.AsyncMessagePojo.ClientMessagePojo;
 
+import controllers.clientManagement.ClientList;
+import controllers.utils.pojo.AsyncMessagePojo.AsyncMessagePojo;
 import model.Clients;
 import org.json.JSONObject;
 import java.io.Serializable;
+import java.util.List;
+
+/**
+ * Created by LuxiaMars on 11/01/2016.
+ */
+
+public class ClientMessagePojo extends AsyncMessagePojo{
+
+    private String nom;
+    private String prenom;
+    public String email;
+    private String sexe;
+    private String client_id_local;
+    private String date_de_naissance;
+    private String magasin_id;
+    private Long idFidelite;
+    private String idRue;
+    private String idVille;
+    private String idCodePostal;
+    private String magasin;
+
+    public ClientMessagePojo(String nom, String prenom, String email, String sexe, String client_id_local,
+                             String date_de_naissance, String magasin_id, Long idFidelite, String idRue, String idVille,
+                             String idCodePostal, String magasin) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.email = email;
+        this.sexe = sexe;
+        this.client_id_local = client_id_local;
+        this.date_de_naissance = date_de_naissance;
+        this.magasin_id = magasin_id;
+        this.idFidelite = idFidelite;
+        this.idRue = idRue;
+        this.idVille = idVille;
+        this.idCodePostal = idCodePostal;
+        this.magasin = magasin;
+    }
 
 
-public class ClientMessagePojo implements Serializable{
 
-    public void action(JSONObject client)
+    @Override
+    public void action()
     {
-        Clients new_client = new Clients();
+        Clients newClient = new Clients();
         JSONObject jsonClient = new JSONObject();
+        ClientList cl = new ClientList();
+
+        /** Si le mail est deja connu, ne pas ajouter le client et renvoyer sa fiche**/
+
+        String emailClient;
+        emailClient = this.email;
+
+        List<Clients> allEmail = Clients.find.where().eq("email", emailClient).findList();
+
+        if(!allEmail.isEmpty())
+        {
+            Clients existingClient = allEmail.get(0);
+            jsonClient.put("nom", existingClient.getNom());
+            jsonClient.put("prenom", existingClient.getPrenom());
+            jsonClient.put("client_id_local", this.client_id_local);
+            jsonClient.put("magasin_id", existingClient.getMagasin());
+            jsonClient.put("idFidelite", existingClient.getIdFidelite());
+            jsonClient.put("date_de_naissance", existingClient.getDate());
+            jsonClient.put("sexe", existingClient.getSexe());
+            jsonClient.put("email", existingClient.getEmail());
+            jsonClient.put("idRue", existingClient.getIdRue());
+            jsonClient.put("idVille", existingClient.getIdVille());
+            jsonClient.put("idCodePostal", existingClient.getIdCodePostal());
+
+            System.out.println("Client existant");
+
+            existingClient.save();
+            return;
+        }
 
         /** Creation du client **/
 
-        new_client.setNom(client.getString("nom"));
-        new_client.setPrenom(client.getString("prenom"));
-        new_client.setEmail(client.getString("email"));
-        new_client.setMagasin(client.getString("magasin_id"));
-        new_client.setDate(client.getString("date_de_naissance"));
-        new_client.setSexe(client.getString("sexe"));
-        new_client.setIdRue(client.getString("idRue"));
-        new_client.setIdVille(client.getString("idVille"));
-        new_client.setIdCodePostal(client.getString("idCodePostal"));
+        newClient.setNom(this.nom);
+        newClient.setPrenom(this.prenom);
+        newClient.setEmail(this.email);
+        newClient.setMagasin(this.magasin_id);
+        newClient.setDate(this.date_de_naissance);
+        newClient.setSexe(this.sexe);
+        newClient.setIdRue(this.idRue);
+        newClient.setIdVille(this.idVille);
+        newClient.setIdCodePostal(this.idCodePostal);
 
 
         /** Creation du JSON a renvoye **/
 
-        jsonClient.put("nom", new_client.getNom());
-        jsonClient.put("prenom", new_client.getPrenom());
-        jsonClient.put("client_id_local", client.getString("client_id_local"));
-        jsonClient.put("magasin_id", new_client.getMagasin());
-        jsonClient.put("idFidelite", new_client.getIdFidelite());
-        jsonClient.put("date_de_naissance", new_client.getDate());
-        jsonClient.put("sexe", new_client.getSexe());
-        jsonClient.put("email", new_client.getEmail());
-        jsonClient.put("idRue", new_client.getIdRue());
-        jsonClient.put("idVille", new_client.getIdVille());
-        jsonClient.put("idCodePostal", new_client.getIdCodePostal());
+        jsonClient.put("nom", newClient.getNom());
+        jsonClient.put("prenom", newClient.getPrenom());
+        jsonClient.put("client_id_local", this.client_id_local);
+        jsonClient.put("magasin_id", newClient.getMagasin());
+        jsonClient.put("idFidelite", newClient.getIdFidelite());
+        jsonClient.put("date_de_naissance", newClient.getDate());
+        jsonClient.put("sexe", newClient.getSexe());
+        jsonClient.put("email", newClient.getEmail());
+        jsonClient.put("idRue", newClient.getIdRue());
+        jsonClient.put("idVille", newClient.getIdVille());
+        jsonClient.put("idCodePostal", newClient.getIdCodePostal());
 
-        new_client.save();
+        newClient.save();
+        cl.addToClientList(jsonClient);
 
-        /** Pretryprint a enlever **/
+        /** PrettyPrint a enlever **/
 
-        System.out.println(new_client.getNom());
-        System.out.println(new_client.getPrenom());
-        System.out.println(new_client.getEmail());
-        System.out.println(new_client.getDate());
-        System.out.println(new_client.getSexe());
-        System.out.println(new_client.getMagasin());
-        System.out.println(new_client.getIdRue());
-        System.out.println(new_client.getIdVille());
-        System.out.println(new_client.getIdCodePostal());
+        System.out.println(newClient.getNom());
+        System.out.println(newClient.getPrenom());
+        System.out.println(newClient.getEmail());
+        System.out.println(newClient.getDate());
+        System.out.println(newClient.getSexe());
+        System.out.println(newClient.getMagasin());
+        System.out.println(newClient.getIdRue());
+        System.out.println(newClient.getIdVille());
+        System.out.println(newClient.getIdCodePostal());
         System.out.println("-----------------------");
-        System.out.println(new_client.getIdFidelite());
-        System.out.println(new_client.getUserId());
+        System.out.println(newClient.getIdFidelite());
+        System.out.println(newClient.getUserId());
     }
+
 }

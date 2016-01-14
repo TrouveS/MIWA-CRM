09f559fd957548d868ff9d093d4807d6eb8b74b6
AmdouniNajla <naj.amdouni@gmail.com>
2016-01-14 17:45:45 +0100

    supprime doublons

--- app/controllers/utils/pojo/AsyncMessagePojo/Monetarysystem/IncidentPojo.java
+++ app/controllers/utils/pojo/AsyncMessagePojo/Monetarysystem/IncidentPojo.java
@@ -39,30 +39,6 @@ public class IncidentPojo extends AsyncMessagePojo {
     public IncidentPojo() {
     }
 
-    public Long getIdfidelite() {
-        return idfidelite;
-    }
-
-    public void setIdfidelite(Long idfidelite) {
-        this.idfidelite = idfidelite;
-    }
-
-    public int getValeur_incident() {
-        return valeur_incident;
-    }
-
-    public void setValeur_incident(int valeur_incident) {
-        this.valeur_incident = valeur_incident;
-    }
-
-    public String getErreur() {
-        return erreur;
-    }
-
-    public void setErreur(String erreur) {
-        this.erreur = erreur;
-    }
-
     public void action(){
         Clients client;
         client = Clients.find.where().eq("idFidelite", idfidelite).findUnique();
